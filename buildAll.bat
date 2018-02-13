37c44f892d61a001e8ef69e0398b50d80e15724c
carlomendowza <mendoza_carlo05@yahoo.com>
2018-02-13 23:51:24 +0800

    Modified buildAll.bat

--- buildAll.bat
+++ buildAll.bat
@@ -2,8 +2,10 @@
 rem javac -cp ./+libs/* -d ./out/ *.java
 rem pause
 
+
 IF NOT EXIST "%CD%"\out\ (
-	ECHO out does not exist, creating . . .
+	ECHO log and out does not exist, creating . . .
+	MD log
 	MD out
 	javac -cp ./lib/* -d ./out/ src/*.java
 	PAUSE
